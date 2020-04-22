c339d39463ed323d61f512fc686ea36219b87da5
ksykes <kaitlyn.sykes@me.com>
2020-04-22 19:54:02 -0400

    updated initial styling

--- template.php
+++ template.php
@@ -8,6 +8,16 @@
 // 	print $element['#children'];
 // }
 
-// function magazine_preprocess_html(&$variables) {
-// 	drupal_add_js('//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5a96f0c85e816089', 'external');
-// }
+// load social links js file
+function romathome_preprocess_html(&$variables) {
+    drupal_add_js('//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5a96f0c85e816089', 'external');
+}
+
+// render related series content block
+function block_render($views, $rom_at_home_series_block) {
+    $block = block_load($views, $rom-at-home-series-block);
+    $block_content = _block_render_blocks(array($block));
+    $build = _block_get_renderable_array($block_content);
+    $block_rendered = drupal_render($build);
+    return $block_rendered;
+}
--- templates/node--rom-at-home-activity.tpl.php
+++ templates/node--rom-at-home-activity.tpl.php
@@ -19,35 +19,34 @@
 
 	<div class="content"<?php print $content_attributes; ?>>
 
-		<?php if (isset($content['field_long_subtitle'])) {
-			print render($content['field_long_subtitle']);
-		} ?>
-		
-		<div class="byline vcard">
-			<?php if (isset($content['field_author_name'])) {
-				print render($content['field_author_name']);
-			} ?>			
-			<?php if (isset($content['field_posting_date'])) {
-				print render($content['field_posting_date']);
-			} ?>
+		<div class="activity-metadata">
+			<div class="activity-tags">
+				<?php if (isset($content['field_rah_activity_type'])) {
+					print render($content['field_rah_activity_type']);
+				} ?>
+	
+				<?php if (isset($content['field_series'])) {
+					print render($content['field_series']);
+				} ?>
+			</div>
+			
 			<div class="addthis_inline_share_toolbox"></div>
 		</div>
 		
-		<?php if (isset($content['field_lead_image'])) {
-			print render($content['field_lead_image']);
-		} ?>		
-		
 		<?php if (isset($content['field_activity_components'])) {
 			print render($content['field_activity_components']);
-		} ?>		
-		
-		<?php if (isset($content['field_related_content'])) {
-			print render($content['field_related_content']);
 		} ?>
-		
-		<?php if (isset($content['field_article_footer'])) {
-			print render($content['field_article_footer']);
-		} ?>
+
+		<div class="related-content">
+			<?php if (isset($content['field_related_content'])) {
+				print render($content['field_related_content']);
+			} ?>
+
+			<!-- if series field is set, print related series content block -->
+			<!-- <?php if (isset($content['field_series'])) {
+				print block_render('views', 'rom-at-home-series-block');
+			} ?> -->
+		</div>
   
 	</div>
 
